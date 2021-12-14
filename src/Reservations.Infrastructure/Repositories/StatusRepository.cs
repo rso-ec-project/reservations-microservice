0b105ebcbd45d76f08573fc9d434fe09883a2536
Anzx <31044008+anzetomazin@users.noreply.github.com>
2021-12-14 13:39:54 +0100

    Implement repositories

---
+++ src/Reservations.Infrastructure/Repositories/StatusRepository.cs
@@ -0,0 +1,11 @@
+﻿using Reservations.Domain.StatusAggregate;
+
+namespace Reservations.Infrastructure.Repositories
+{
+    public class StatusRepository : Repository<Status, int>, IStatusRepository
+    {
+        public StatusRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
