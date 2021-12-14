cd2b56a007945e637a67cfa921b9f00295db9ff0
Anzx <31044008+anzetomazin@users.noreply.github.com>
2021-12-14 13:17:56 +0100

    Create DB context & inject into services

--- src/Reservations.API/Startup.cs
+++ src/Reservations.API/Startup.cs
@@ -1,16 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Reservations.Infrastructure;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Reservations.API
 {
@@ -26,12 +22,26 @@ namespace Reservations.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                options.UseNpgsql(GetConnectionString());
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Reservations.API", Version = "v1" });
             });
+        }
+
+        private string GetConnectionString()
+        {
+            var host = Environment.GetEnvironmentVariable("DB_HOST");
+            var database = Environment.GetEnvironmentVariable("DB_NAME");
+            var username = Environment.GetEnvironmentVariable("DB_USERNAME");
+            var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+            return $"Host={host};Database={database};Username={username};Password={password}";
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
