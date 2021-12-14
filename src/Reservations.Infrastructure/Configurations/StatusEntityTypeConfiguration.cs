33caa3c46e8fabd410c7f6779823787250bda4a2
Anzx <31044008+anzetomazin@users.noreply.github.com>
2021-12-14 13:16:14 +0100

    Create EntityType Configurations

---
+++ src/Reservations.Infrastructure/Configurations/StatusEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Reservations.Domain.StatusAggregate;
+
+namespace Reservations.Infrastructure.Configurations
+{
+    public class StatusEntityTypeConfiguration : IEntityTypeConfiguration<Status>
+    {
+        public void Configure(EntityTypeBuilder<Status> builder)
+        {
+            builder.ToTable("status");
+
+            builder.Property(x => x.Name)
+                .HasColumnName("name")
+                .IsRequired()
+                .HasMaxLength(255);
+        }
+    }
+}
