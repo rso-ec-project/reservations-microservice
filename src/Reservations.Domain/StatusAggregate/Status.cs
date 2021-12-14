74906520815d8630932a14d0af43149e498a518b
Anzx <31044008+anzetomazin@users.noreply.github.com>
2021-12-14 13:15:17 +0100

    Create entity classes

---
+++ src/Reservations.Domain/StatusAggregate/Status.cs
@@ -0,0 +1,13 @@
+﻿using Reservations.Domain.ReservationAggregate;
+using Reservations.Domain.Shared.Entities;
+using System.Collections.Generic;
+
+namespace Reservations.Domain.StatusAggregate
+{
+    public class Status : Entity<int>
+    {
+        public string Name { get; set; }
+
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
