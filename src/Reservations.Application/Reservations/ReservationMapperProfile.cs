85e738080117bdd94af9876c6f8ca8e6d0599f69
Anzx <31044008+anzetomazin@users.noreply.github.com>
2021-12-28 13:35:37 +0100

    Set default Status when new Reservation is created

--- src/Reservations.Application/Reservations/ReservationMapperProfile.cs
+++ src/Reservations.Application/Reservations/ReservationMapperProfile.cs
@@ -20,10 +20,10 @@ namespace Reservations.Application.Reservations
             CreateMap<ReservationPostDto, Reservation>()
                 .ForMember(dest => dest.From, opts => opts.MapFrom(src => src.From))
                 .ForMember(dest => dest.To, opts => opts.MapFrom(src => src.To))
-                .ForMember(dest => dest.Code, opts => opts.MapFrom(src => src.Code))
+                .ForMember(dest => dest.Code, opts => opts.MapFrom(src => ""))
                 .ForMember(dest => dest.UserId, opts => opts.MapFrom(src => src.UserId))
                 .ForMember(dest => dest.ChargerId, opts => opts.MapFrom(src => src.ChargerId))
-                .ForMember(dest => dest.StatusId, opts => opts.MapFrom(src => src.StatusId))
+                .ForMember(dest => dest.StatusId, opts => opts.MapFrom(src => 1))
                 ;
         }
     }
--- src/Reservations.Application/Reservations/ReservationPostDto.cs
+++ src/Reservations.Application/Reservations/ReservationPostDto.cs
@@ -6,9 +6,7 @@ namespace Reservations.Application.Reservations
     {
         public DateTime From { get; set; }
         public DateTime To { get; set; }
-        public string Code { get; set; }
         public int UserId { get; set; }
         public int ChargerId { get; set; }
-        public int StatusId { get; set; }
     }
 }
