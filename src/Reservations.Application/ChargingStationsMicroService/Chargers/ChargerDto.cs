1b875a3e547ef9f84b105032504a3c3e71ff0ca7
Anzx <31044008+anzetomazin@users.noreply.github.com>
2021-12-28 16:41:53 +0100

    Add ChargingStationsMS connection

---
+++ src/Reservations.Application/ChargingStationsMicroService/Chargers/ChargerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Reservations.Application.ChargingStationsMicroService.Chargers
+{
+    public class ChargerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double ChargingFeePerKwh { get; set; }
+        public string ModelName { get; set; }
+        public string Manufacturer { get; set; }
+    }
+}
